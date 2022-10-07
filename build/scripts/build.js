c752602604d7c36a8cc9c6d0959ce3ce4052f201
Vuk Petrovic <vuk.petrovic@quotapath.com>
2022-10-07 15:20:15 -0400

    Updated project to bundle types files

--- build/scripts/build.js
+++ build/scripts/build.js
@@ -3,6 +3,7 @@ import { writeFileSync, readdirSync, unlinkSync, renameSync } from "fs";
 import { makeBadge } from "badge-maker";
 import { __dirname, EXIT_SUCCESS, EXIT_FAILURE } from "./shared.js";
 import { rollup } from "rollup";
+import dts from "rollup-plugin-dts";
 
 const buildBadgeOptions = {
   label: "build",
@@ -14,6 +15,36 @@ console.log("Running TypeScript compiler...");
 let exitStatus = EXIT_SUCCESS;
 
 const root = `${__dirname}/../../`;
+
+async function bundle(inputFile, outputFile, outputFormat) {
+  try {
+    const bundle = await rollup({
+      input: inputFile,
+      plugins: [...(inputFile.includes(".d.ts") ? [dts()] : [])],
+    });
+
+    try {
+      await bundle.write({
+        file: outputFile,
+        format: outputFormat,
+      });
+    } catch (writeError) {
+      console.error(`Failed to write to ${outputFile}:\n${writeError}`);
+      exitStatus = EXIT_FAILURE;
+    }
+  } catch (e) {
+    console.error(`Failed to bundle ${inputFile}:\n${e}`);
+    exitStatus = EXIT_FAILURE;
+  }
+
+  if (exitStatus === EXIT_FAILURE) {
+    buildBadgeOptions.message = "failing";
+    buildBadgeOptions.color = "red";
+
+    writeFileSync("./status/build.svg", makeBadge(buildBadgeOptions));
+    process.exit(exitStatus);
+  }
+}
 
 exec("tsc", { cwd: root }, async (buildError, _, __) => {
   if (buildError) {
@@ -28,43 +59,9 @@ exec("tsc", { cwd: root }, async (buildError, _, __) => {
 
   console.log("Bundling...");
 
-  try {
-    const bundle = await rollup({ input: `${root}/build/index.js` });
-
-    try {
-      await bundle.write({
-        file: `${root}/dist/index.js`,
-        format: "es",
-      });
-    } catch (writeError) {
-      console.log(`Could not persist bundle to disk:\n${writeError}`);
-      exitStatus = EXIT_FAILURE;
-    }
-  } catch (e) {
-    console.error(`Module bundling failed:\n${e}`);
-    exitStatus = EXIT_FAILURE;
-  }
-
-  if (exitStatus === EXIT_FAILURE) {
-    buildBadgeOptions.message = "failing";
-    buildBadgeOptions.color = "red";
-  } else {
-    console.log("BUILD SUCCEEDED");
-  }
+  await bundle(`${root}/build/index.js`, `${root}/dist/index.js`, "es");
+  await bundle(`${root}/build/dts/index.d.ts`, `${root}/dist/index.d.ts`, "es");
 
   writeFileSync("./status/build.svg", makeBadge(buildBadgeOptions));
-
-  // if (exitStatus === EXIT_FAILURE) process.exit(exitStatus);
-
-  // const distPath = `${root}/dist`;
-  // const fNamePattern = /[.]js$/;
-
-  // console.log("Cleaning up...");
-  // readdirSync(distPath)
-  //   .filter((fileName) => fNamePattern.test(fileName))
-  //   .map((fileName) => unlinkSync(`${distPath}/${fileName}`));
-
-  // renameSync(`${distPath}/bundle`, `${distPath}/index.js`);
-
   process.exit(EXIT_SUCCESS);
 });
